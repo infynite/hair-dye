ae1a2e25b0589da913dcaa33387fe501e40464aa
infynite <44844809+infynite@users.noreply.github.com>
2020-12-06 22:02:19 +0330

    Download and Clean data from main source

--- download.sh
+++ download.sh
@@ -1,27 +1,17 @@
 #!/bin/bash
 
-echo "====================="
-echo "Create data directory"
 mkdir data
-echo "====================="
-echo "Into data directory"
 cd data
-
-echo "====================="
-echo "Download Figaro1K dataset from google drive"
-gdown https://drive.google.com/uc?id=1MTvPCpkzF0Gb5JhFdxRlDUwu6Cll2gdS
-
-echo "====================="
-echo "Decompression dataset.tar"
-tar zxvf dataset.tar
-
-echo "====================="
-echo "remove unnecessary file"
-rm dataset_figaro1k/training/images/._Frame*
-rm dataset_figaro1k/training/masks/._Frame*
-rm dataset_figaro1k/testing/images/._Frame*
-rm dataset_figaro1k/testing/masks/._Frame*
-
-echo "====================="
-echo "complete!!"
-echo "====================="
+wget https://projects.i-ctm.eu/sites/default/files/AltroMateriale/207_Michele%20Svanera/Figaro1k.zip
+unzip Figaro1k.zip
+mkdir -p dataset_figaro1k/testing/images
+mkdir -p dataset_figaro1k/testing/masks
+mkdir -p dataset_figaro1k/training/images
+mkdir -p dataset_figaro1k/training/masks
+cp -r Figaro1k/GT/Training/*.pbm dataset_figaro1k/training/masks/
+cp -r Figaro1k/Original/Training/*.jpg dataset_figaro1k/training/images/
+cp -r Figaro1k/GT/Testing/*.pbm dataset_figaro1k/testing/masks/
+cp -r Figaro1k/Original/Testing/*.jpg dataset_figaro1k/testing/images/
+rm -rf Figaro1k
+rm -rf __MACOSX/
+rm Figaro1k.zip
